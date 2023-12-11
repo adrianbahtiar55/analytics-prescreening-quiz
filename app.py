3863dcbb27c024483cc818254a1d37121a354eca
Adrian Bahtiar <adrian@bahtiar.id>
2023-12-11 05:22:31 -0600

    changes

--- app.py
+++ app.py
@@ -1,28 +1,37 @@
 from flask import Flask, render_template, request
+import requests
 
 app = Flask(__name__)
 
+WEBHOOK = 'https://maker.ifttt.com/trigger/quiz/json/with/key/hxHyMyXbPqHP4etuR9rbSGHofOd5p80C2PC4cyWjPqQ'
+
 @app.route('/')
 def home():
-    # Attempt to open and write to a file
-    try:
-        with open('test_file.txt', 'a') as file:
-            file.write('This is a test.')
-        write_permission = True
-    except Exception as e:
-        write_permission = False
-
     return render_template('quiz.html')
 
 @app.route('/submit_quiz', methods=['POST'])
 def submit_quiz():
     try:
         data = request.form
-        with open('submissions.txt', 'a') as file:
-            file.write(f"{data['name']} | {data['email']} | {data['correctAnswers']} | {data['timeTaken']} | {data['tabChanges']}\n")
-        return "Submission recorded"
-    except IOError as e:
-        return f"Error writing to file: {e}", 500
+        # Send data to webhook
+        payload = {
+            'name': data['name'],
+            'email': data['email'],
+            'correctAnswers': data['correctAnswers'],
+            'timeTaken': data['timeTaken'],
+            'tabChanges': data['tabChanges']
+                }
+        headers = {'Content-Type': 'application/json'}  # Set the Content-Type header
+        response = requests.post(WEBHOOK, json=payload, headers=headers)
+
+        # Check if the request was successful
+        if response.status_code == 200:
+            return "Submission recorded and sent to Zapier"
+        else:
+            return "Error sending data to Zapier", 500
+
+    except Exception as e:
+        return f"Error: {str(e)}", 500
 
 
 @app.route('/submissions')
